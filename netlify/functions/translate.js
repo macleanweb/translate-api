038a527f7521b4842134836ac0e47bba65ced0d0
Andrew MacLean <andrew.maclean@helloprint.com>
2023-06-21 13:38:19 +0200

    add netlify func

---
+++ netlify/functions/translate.js
@@ -0,0 +1,28 @@
+exports.handler = async function(event, context) {
+  try {
+    const { text, targetLanguage } = JSON.parse(event.body);
+    const API_KEY = process.env.DEEPL_API_KEY;
+
+    const response = await fetch(
+      `https://api-free.deepl.com/v2/translate?auth_key=${API_KEY}&text=${encodeURIComponent(
+        text
+      )}&target_lang=${targetLanguage}`
+    );
+
+    if (!response.ok) {
+      throw new Error('Translation request failed');
+    }
+
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
