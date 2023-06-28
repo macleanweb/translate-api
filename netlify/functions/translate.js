ac361b347a923e0cce81a05e4def4171ed9b9863
Andrew MacLean <andrew.maclean@helloprint.com>
2023-06-28 15:51:36 +0200

    debugging

--- netlify/functions/translate.js
+++ netlify/functions/translate.js
@@ -1,20 +1,27 @@
+import * as deepl from 'deepl-node';
+
 exports.handler = async function(event, context) {
   console.log(event, context);
   try {
-    const { text, targetLanguage } = JSON.parse(event.body);
-    const API_KEY = process.env.DEEPL_API_KEY;
+    const payload = JSON.parse(event.body);
+    console.log(payload);
+    // const API_KEY = process.env.DEEPL_API_KEY;
+    // const urlSearchParams = new URLSearchParams();
 
-    const response = await fetch(
-      `https://api-free.deepl.com/v2/translate?auth_key=${API_KEY}&text=${encodeURIComponent(
-        text
-      )}&target_lang=${targetLanguage}`
-    );
+    // Object.keys(payload).forEach(key => {
+    //     urlSearchParams.append(key, payload[key]);
+    // });
 
-    if (!response.ok) {
-      throw new Error('Translation request failed');
-    }
+    // const urlEncodedBody = urlSearchParams.toString();
+    // const url = `https://api-free.deepl.com/v2/translate?auth_key=${API_KEY}&${urlEncodedBody}`;
 
-    const data = await response.json();
+    // const response = await fetch(url);
+
+    // if (!response.ok) {
+    //   throw new Error('Translation request failed');
+    // }
+
+    // const data = await response.json();
 
     return {
       statusCode: 200,
