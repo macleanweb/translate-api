b0e3bad8ea6762dc1680345c80d2cda079a1f54b
Andrew MacLean <andrew.maclean@helloprint.com>
2023-06-28 18:23:09 +0200

    troubleshoot headers not sent

--- netlify/functions/translate.js
+++ netlify/functions/translate.js
@@ -19,6 +19,11 @@ exports.handler = async function(event) {
 
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
       body: JSON.stringify(data),
     };
   } catch (error) {
